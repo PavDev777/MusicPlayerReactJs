bb211f39f3a434b3b2e54740aa3b92135a026afa
Pavel <cjbassfly@gmail.com>
2020-12-16 17:19:16 +0600

    _

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ import { faMusic } from "@fortawesome/free-solid-svg-icons";
 const Nav = ({ setLibraryStatus, libraryStatus }) => {
   return (
     <nav>
-      <h1>Super Puper music player</h1>
+      <h1>Music player</h1>
       <button onClick={() => setLibraryStatus(!libraryStatus)}>
         Open
         <FontAwesomeIcon icon={faMusic} />
--- src/data.js
+++ src/data.js
@@ -31,6 +31,16 @@ function chillHop() {
       color: ["#BA4A46", "#FDF0DD"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11245",
     },
+    {
+      name: "Покушать принес",
+      cover: "https://vk.vkfaces.com/851520/v851520803/11e878/-QVE-p8D-WY.jpg",
+      artist: "Пахом, Вова, Рустам",
+      id: uuidv4(),
+      active: false,
+      color: ["#BA4A46", "#FDF0DD"],
+      audio:
+        "https://data.ipleer.com/file/121295032/c2ZxcHA1cEMxZlRYS0dNaGc5dmE5WDluMHN3OVZBZFBINng4NldMMUZ2U0JmdTU0eGdYTW1GeS9WN25yWExrSGYzOFI4K3NPQWNEOFFnWG5FOUp6SXJzNzZHZHdFUGhiMzdmaTg2c2R3MDFpZFNSd3BvNElSbWFtemp3K3c3Q0Y/Zelyonyj_-_Slonik_(iPleer.com).mp3",
+    },
   ];
 }
 export default chillHop;
